1346900a2bfddf4cd1a5b52ba97cedf9ef4e4cae
ahmed salama <ahmed.salama.fci2015@gmail.com>
2020-11-02 12:04:31 +0200

    end of 7

--- ZwajApp-SPA/src/app/app.module.ts
+++ ZwajApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule   } from '@angular/common/http';
 import {FormsModule   } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
  import { NavComponent } from './nav/nav.component';
@@ -11,21 +13,30 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvidor } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
-  declarations: [				
+  declarations: [							
     AppComponent,
        NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [AuthService,ErrorInterceptorProvidor,AlertifyService],
+  providers: [AuthService,ErrorInterceptorProvidor,AlertifyService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ZwajApp-SPA/src/app/nav/nav.component.ts
+++ ZwajApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import {  AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,14 +12,15 @@ export class NavComponent implements OnInit {
 
 model:any={};
 
-  constructor(public  authService:AuthService,private alertify:AlertifyService) { }
+  constructor(private router:Router,public  authService:AuthService,private alertify:AlertifyService) { }
 
   ngOnInit() {
   }
 login(){
   this.authService.login(this.model).subscribe(
     next=>{this.alertify.success('تم الدخول') },
-    err=>{this.alertify.error(err)}
+    err=>{this.alertify.error(err)},
+    ()=>{this.router.navigate(['/members'])}
   );
   console.log(this.model);
 }
@@ -33,7 +35,7 @@ loggedOut(){
   localStorage.removeItem('token');
    this.alertify.message('تم الخروج') ;
  
- 
+   this.router.navigate(['/home']);
  
 }
 
